3790f1c7854acaee0aca42753f48a43137847ea9
Sergio Roman <sergio@nura.app>
2026-06-17 14:12:58 +0000

    Fix crítico: import HelperCard en Explore.jsx (error 'Can't find variable: HelperCard')

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,3 +1,4 @@
+import HelperCard from '../components/HelperCard'
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Search, SlidersHorizontal, X } from 'lucide-react'
